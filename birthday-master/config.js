b43baa2ede1746ebfd51eed15367965694cccacd
Hoang Dinh Tung <hoangtung031103@gmail.com>
2023-06-24 20:31:04 +0700

    first commit

---
+++ birthday-master/config.js
@@ -0,0 +1,42 @@
+// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "Chúc bạn sinh nhật vui vẻ",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "心爱的小可爱",  // 同上...
+        "hôm nay là sinh nhật của bạn",
+        "tuổi mới xinh lại thêm xinh",
+        "duyên lại càng thêm duyên",
+        "và yêu đời nhé!",
+        " Có thể cả thế giới không nhớ",
+        "không để tâm đến ngày của bạn nhưng mình luôn nhớ, biết rất rõ về ngày sinh nhật của bạn",
+        "Chúc bạn của tớ sinh nhật vui vẻ và tràn ngập hạnh phúc",
+        "Happy Birthday",
+        "Đinh Thị Liệp",
+    ],
+    /**
+     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+     * 例如
+     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     *
+     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+     */
+    imgs: {
+        "心爱的小可爱": "./imgs/quynh_nhu.jpg",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "click vào đây nhé",
+        play: "tiếp tục click vào đây nhé",
+        bannar_coming: "tiếp tục click vào đây nhé",
+        balloons_flying: "Có vẻ như thiếu một cái gì đó",
+        cake_fadein: "Có vẻ như vẫn thiếu một cái gì đó",
+        light_candle: "tiếp tục click vào đây nhé",
+        wish_message: "Chúc mừng sinh nhật và xin chào tuổi 20+",
+        story: "A MESSAGE FOR YOU",
+    }
+};
